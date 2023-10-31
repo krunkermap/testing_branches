e886ec7bc1474865d7b717e81f3013550de5b6a5
tstef87 <ts719104@sju.edu>
2023-10-31 11:48:43 -0400

    test

--- src/testing.java
+++ src/testing.java
@@ -1,5 +1,7 @@
 public class testing {
     public static void main (String [] args){
-        System.out.println("Troy");
+        for (int i = 0; i < 5; i++ ){
+            System.out.println("troy");
+        }
     }
 }
